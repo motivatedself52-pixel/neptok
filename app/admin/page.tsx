8264704882250d4647c4d463724e8045710aebfb
motivatedself52-pixel <motivatedself52@gmail.com>
2026-09-18 10:05:24 +0545

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Navbar from "@/components/Navbar";
+import Navbar from "../components/Navbar";
 
 export default function AdminPage() {
   const [pin, setPin] = useState("");
